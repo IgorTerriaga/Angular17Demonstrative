1393911325c67da918d623748784b4f8108a3e04
IgorTerriaga <igsantos1996@gmail.com>
2024-05-28 11:12:42 -0300

    add lazyloading page create

--- src/app/feature/list/list.component.ts
+++ src/app/feature/list/list.component.ts
@@ -2,11 +2,13 @@ import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/IProduct';
 import { CardComponent } from '../../features/list/components/card/card.component';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, MatButtonModule, RouterLink],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
